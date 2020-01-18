024eefd11a5c25bc56ab3ed95de0f4ad1e8f47e2
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2020-01-18 15:41:45 +0300

    done

--- 02-01.js
+++ 02-01.js
@@ -1,59 +1,13 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-const express = require("express");
-const app = express();
+http.createServer(function (request, response) {
+    if(url.parse(request.url).pathname === '/html') {
+        let html = fs.readFileSync('./index.html');
+        response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        response.end(html);
+    }
+}).listen(5000);
 
-app.get("/html", function(request, response){
-    let html = fs.readFileSync('./index.html');
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end(html);
-});
-
-app.get("/png", function(request, response){
-    const fname = './pic.png';
-    let jpg = null;
-
-    fs.stat(fname, (err, stat)=>{
-        if(err){console.log('error:',err);}
-        else {
-            jpg = fs.readFile(fname, (err, data)=>{
-                response.contentType('image/jpeg');
-                response.contentLenght = stat.size;
-                response.end(data, 'binary');
-            });
-        }
-    });
-});
-
-app.get("/api/name", function(request, response){
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Maximchikova Yuliya Sergeevna');
-});
-
-app.get("/xmlhttprequest", function(request, response){
-    let html = fs.readFileSync('./xmlhttprequest.html');
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end(html);
-});
-
-app.get("/fetch", function(request, response){
-    let html = fs.readFileSync('./fetch.html');
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end(html);
-});
-
-app.get("/jquery", function(request, response){
-    let html = fs.readFileSync('./jquery.html');
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end(html);
-});
-
-app.get("/", function(request, response){
-    response.statusCode = 404;
-    response.end();
-});
-
-app.listen(5000);
-
-console.log('Server running at http://localhost:5000/');
+console.log('Server running at http://localhost:5000/html');
